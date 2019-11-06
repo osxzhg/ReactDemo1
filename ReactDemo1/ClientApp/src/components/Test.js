ce57b741905957b098ad23d58a02f91a3c4bdb1b
osxzhg <osxzhg@gmail.com>
2019-11-06 19:14:37 +1300

    2019/11/6

--- ReactDemo1/ClientApp/src/components/Test.js
+++ ReactDemo1/ClientApp/src/components/Test.js
@@ -1,79 +1,99 @@
 ﻿import React, { Component } from 'react';
 
-export class  Test extends Component {
+export class Test extends Component {
     displayName = Test.name
 
     constructor(props) {
         super(props);
-        this.state = { currentCount: 0 };
-        this.incrementCounter = this.incrementCounter.bind(this);
+        this.state = { customers: []};
+        this.loadData = this.loadData.bind(this);
+        this.update = this.update.bind(this);
+        this.delete = this.delete.bind(this);
+
+        //fetch('api1/Customer/Index')
+        //    .then(response => response.json())
+        //    .then(data => {
+        //        this.setState({ customers: data, loading: false });
+        //    });
     }
 
-    incrementCounter() {
-        this.setState({
-            currentCount: this.state.currentCount + 1
-        });
+    //static renderCustomersTable(customers) {
+    //    return (
+    //        <table className='table'>
+    //            <thead>
+    //                <tr>
+    //                    <th>Name</th>
+    //                    <th>Address</th>
+    //                </tr>
+    //            </thead>
+    //            <tbody>
+    //                {customers.map(customer =>
+    //                    <tr key={customer.Id}>
+    //                        <td>{customer.Name}</td>
+    //                        <td>{customer.Address}</td>
+    //                    </tr>
+    //                )}
+    //            </tbody>
+    //        </table>
+    //    );
+    //}
+
+    componentDidMount() {
+        this.loadData();
     }
 
-    handleClick(e) {
-        e.preventDefault();
-        console.log("The link was clicked");
+    loadData() {
+        //ajax call logic
+        fetch('api/Customers/GetCustomers')
+            .then(response => { console.log(response); response.json() })
+            .then(data => {
+                this.setState({ customers: data, loading: false });
+            });
+    }
+
+    update(id) {
+        //ajax call logic
+    }
+
+    delete(id) {
+        //ajax call logic
     }
 
     render() {
+        let customers = this.state.customers;
+
+        let tableData = null;
+
+        if (customers != "") {
+            tableData = customers.map(customer =>
+                <tr key={customer.id}>
+                    <td className="two wide">{customer.Name}</td>
+                    <td className="ten wide">{customer.Address}</td>
+                    <td className="four wide">
+                        <i className="outline write icon" onClick={
+                            this.update.bind(this, customer.id)}></i>
+                        <i className="remove icon" onClick=
+                            {this.delete.bind(this, customer.id)}></i>
+                    </td>
+                </tr>
+            )
+        }
         return (
-            <div>
-                <h1>Counter</h1>
+            <React.Fragment>
+                <table className="ui striped table">
+                    <thead>
+                        <tr>
+                            <th className="two wide">Title</th>
+                            <th className="ten wide">Description</th>
+                            <th className="four wide">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tableData}
+                    </tbody>
+                </table>
+            </React.Fragment>
+        )
 
-                <p>This is a simple example of a React component.</p>
-
-                <p>Current count: <strong>{this.state.currentCount}</strong></p>
-
-                <button onClick={this.incrementCounter}>Increment</button>
-
-                <p>
-                    The story of how the barge came to rest just above the falls is part of local lore. It involves the rescue of two men from nearby Buffalo, according to Hill, who is the senior manager of heritage with the Parks Commission.
-            In 1918, a vessel known as a dumping scow became disconnected from its tug boat — with two men aboard — during a dredging operation, according to the Commission, an agency of the Ontario Ministry of Tourism, Culture and Sport. The scow got stranded in the Niagara River, some 650 yards shy of Horseshoe Falls, one of three separate waterfalls that make up Niagara Falls.
-                </p>
-                <p>
-                    Napoleon never lived to make it off the island, but a new air service from New York is about to make this remote British possession that much easier for international tourists to visit -- and to leave.
-                </p>
-                <p>
-                    United Airlines has introduced a new nonstop flight service from New York/Newark to Cape Town, beginning December 15, 2019, which will be able to connect to new seasonal flights from Cape Town to St Helena operated by regional South African airline Airlink.
-                    </p>
-                <p>
-                    Airlink will operate flights every Tuesday from Cape Town to St Helena from December 3, 2019 to February 25, 2020 (excluding Christmas Eve and New Year's Eve).
-                </p>
-                <p>
-                    That's in addition to Airlink's existing Johannesburg to St Helena flights, which operate every Saturday.
-
-                </p>
-                <p>
-                    Sounds infrequent? Well, until 2017, St Helena didn't have a commercial flight service at all.
-                </p>
-                <p>
-                    Until a few years ago, the island's only regular link to the outside world was RMS St. Helena, the mail ship that, every three weeks, covered the five-and-a-half day journey from Cape Town, South Africa.
-                </p>
-                <p>
-                    In 2016, with much fanfare, St. Helena prepared to inaugurate its first ever airport -- but then an unexpected obstacle got in the way: the South Atlantic winds.
-                </p>
-                <p>
-                    South African airline Comair, a British Airways franchise, planned to start flights to the island in May 2016 using a Boeing 737-800 aircraft. However, a serious wind shear issue was detected as soon as the airliner attempted to land.
-                </p>
-
-                <p>
-                    South African airline Comair, a British Airways franchise, planned to start flights to the island in May 2016 using a Boeing 737-800 aircraft. However, a serious wind shear issue was detected as soon as the airliner attempted to land.
-                </p>
-                <p>
-                    South African airline Comair, a British Airways franchise, planned to start flights to the island in May 2016 using a Boeing 737-800 aircraft. However, a serious wind shear issue was detected as soon as the airliner attempted to land.
-                </p>
-                <p>
-                    South African airline Comair, a British Airways franchise, planned to start flights to the island in May 2016 using a Boeing 737-800 aircraft. However, a serious wind shear issue was detected as soon as the airliner attempted to land.
-                </p>
-                <a href="#" onClick={this.handleClick}>
-                    Click me
-                </a>
-            </div>
-        );
     }
 }
--- ReactDemo1/Startup.cs
+++ ReactDemo1/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ReactDemo1.Models;
 
 namespace ReactDemo1
 {
@@ -21,7 +23,8 @@ namespace ReactDemo1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            services.AddDbContext<OnbordingContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
